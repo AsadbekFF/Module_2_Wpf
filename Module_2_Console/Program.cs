65177c4e7e7f979ad0d4f29c4e011e2e1936abee
AsadbekFF <79692640+AsadbekFF@users.noreply.github.com>
2021-09-25 18:44:33 +0500

    Initial commit

---
+++ Module_2_Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Module_2_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Module_2_Library/Task2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module_2_Library
+{
+    public class Task2
+    {
+        public string GetName(string name)
+        {
+            return $"{DateTime.Now} Hello, {name}!";
+        }
+    }
+}
